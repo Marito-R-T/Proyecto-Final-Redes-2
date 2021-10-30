6d7bc054e217ec857101ece9cc4d5bc46ace6e97
Marito-R-T <mariomoises20008@gmail.com>
2021-10-30 17:54:37 -0600

    cambios

--- modulos/comandos-isp.sh
+++ modulos/comandos-isp.sh
@@ -6,10 +6,30 @@ INTERFACEOUT="ifb0"
 IP="${isp}0.${isp}0.${isp}0.1"
 
 #filtros para aplicar el ancho de banda de subida como de bajada
-IN="/usr/sbin/tc filter add dev ${INTERFACEIN} parent 1:0 protocol ip prio 1 u32 match ip dst"
-OUT="/usr/sbin/tc filter add dev ${INTERFACEOUT} parent 1:0 protocol ip prio 1 u32 match ip dst"
+IN="/dev/sbin/tc filter add dev ${INTERFACEIN} parent 1:0 protocol ip prio 1 u32 match ip dst"
+OUT="/dev/sbin/tc filter add dev ${INTERFACEOUT} parent 1:0 protocol ip prio 1 u32 match ip src"
 PUERTO="match ip sport"
 REGLA="0Xffff"
+
+
+ejecutarComandos() {
+  modprobe ifb numifbs=1
+  ip link set dev ${INTERFACEOUT} up
+  /usr/sbin/tc qdisc del dev ${INTERFACEIN} root 2>/dev/null
+  /usr/sbin/tc qdisc del dev ${INTERFACEIN} ingress 2>/dev/null
+  /usr/sbin/tc qdisc del dev ${INTERFACEOUT} root 2>/dev/null
+  /usr/sbin/tc qdisc add dev ${INTERFACEIN} handle ffff: ingress
+  /usr/sbin/tc filter add dev ${INTERFACEIN} parent ffff: protocol ip u32 match u32 0 0 action mirred egress redirect dev ${INTERFACEOUT}
+  /usr/sbin/tc  qdisc add dev ${INTERFACEIN} root handle 1: htb
+  /usr/sbin/tc  class add dev ${INTERFACEIN} parent 1: classid 1:10 htb rate 1000kbit ceil 1000kbit
+  /usr/sbin/tc qdisc add dev ${INTERFACEIN} parent 1:10 handle 10: sfq perturb 10
+  /usr/sbin/tc  qdisc add dev ${INTERFACEOUT} root handle 1: htb
+  /usr/sbin/tc  class add dev ${INTERFACEOUT} parent 1: classid 1:10 htb rate 100kbit ceil 100kbit
+  /usr/sbin/tc qdisc add dev ${INTERFACEOUT} parent 1:10 handle 10: sfq perturb 10
+  /usr/sbin/tc ${IN} ${IP} flowid 1:10
+  /usr/sbin/tc ${OUT} ${IP} flowid 1:10
+}
+
 
 #insertar modulo ifb, asignando el numero de interfaces virutales
 #que se necesita por defecto es 2
