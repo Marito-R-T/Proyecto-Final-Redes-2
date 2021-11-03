e9d067cfbddaf614754cd2328bfa56add7ff2f51
Marito-R-T <mariomoises20008@gmail.com>
2021-11-02 22:26:12 -0600

    cambios

--- conf_squid.sh
+++ conf_squid.sh
@@ -9,10 +9,10 @@ iptables -t mangle -F
 iptables -t mangle -X
 
 # redirect HTTP to locally installed Squid instance
-iptables -t nat -A PREROUTING -i enp0s9 -p tcp --dport 80 -j REDIRECT --to-ports 3127
+iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 80 -j REDIRECT --to-ports 3127
 
 # redirect HTTPS to locally installed Squid instance
-iptables -t nat -A PREROUTING -i enp0s9 -p tcp --dport 443 -j REDIRECT --to-ports 3128
+iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 443 -j REDIRECT --to-ports 3128
 
 
 
